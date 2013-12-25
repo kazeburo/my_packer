ff908158f99902bf1aaf0b3af7ec91c17ef3f39b
Masahiro Nagano <kazeburo@gmail.com>
2013-12-25 13:56:28 +0900

    update

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,7 +6,29 @@ rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
 yum clean metadata
 
+yum -y groupinstall 'Base'
 yum -y groupinstall 'Development Tools'
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget yum-utils
-yum -y install curl bind-utils file git mailx man ntp openssh-clients patch rsync screen sysstat dstat htop traceroute vim-enhanced
+yum -y install curl vim-enhanced
+yum -y update
+
+/sbin/chkconfig iptables off
+/sbin/chkconfig ip6tables off
+/sbin/chkconfig cpuspeed off
+/sbin/chkconfig irqbalance off
+/sbin/chkconfig iscsi off
+/sbin/chkconfig iscsid off
+/sbin/chkconfig auditd off
+/sbin/chkconfig kdump off
+/sbin/chkconfig mdmonitor off
+/sbin/chkconfig messagebus off
+/sbin/chkconfig netconsole off
+/sbin/chkconfig netfs off
+/sbin/chkconfig restorecond off
+/sbin/chkconfig postfix off
+
+reboot
+sleep 60
+
+
+
 
--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,4 +1,5 @@
 # Installing the virtualbox guest additions
+yum -y install kernel-devel-`uname -r`
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
